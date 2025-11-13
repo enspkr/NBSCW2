933e4da6bdfeb503a813c5ef6703e281e4245f50
Rashandd <89455752+Rashandd@users.noreply.github.com>
2025-11-14 01:15:05 +0300

    admin_fix

--- python_version/python_version/urls.py
+++ python_version/python_version/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')), # Kök URL'ye uygulamanızı bağladık
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
